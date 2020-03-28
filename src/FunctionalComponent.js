5ecc3e476b90ab1f486be2275b66572a029e91a8
Harsh Agrawal <agrawalharsh90@gmail.com>
2020-03-28 19:39:09 +0530

    funtional component done

---
+++ src/FunctionalComponent.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import './FunctionalComponent.css';
+
+function Todo({todo, index, completeTodo, removeTodo}) {
+    return (<div className="todo" style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}>
+        {todo.text}
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>X</button>
+        </div>
+    </div>);
+}
+
+function TodoForm({addTodo}) {
+    const [value, setValue] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue('');
+    };
+    return (<form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo"
+            onChange={e => setValue(e.target.value)}/>
+    </form>);
+}
+
+function FunctionalComponent() {
+    const [todos, setTodos] = useState([{
+        text: 'Learn a react',
+        isCompleted: false
+    }, {
+        text: 'Meet a react',
+        isCompleted: false
+    }, {
+        text: 'Learn a react',
+        isCompleted: false
+    },]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, {text}];
+        setTodos(newTodos);
+    };
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
+    };
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo key={index}
+                          index={index}
+                          todo={todo}
+                          completeTodo={completeTodo}
+                          removeTodo={removeTodo}/>
+                ))}
+                <TodoForm addTodo={addTodo}/>
+            </div>
+
+        </div>
+    );
+}
+
+
+export default FunctionalComponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ClassComponent from './ClassComponent';
 import * as serviceWorker from './serviceWorker';
+import FunctionalComponent from "./FunctionalComponent";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ClassComponent />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <FunctionalComponent/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
